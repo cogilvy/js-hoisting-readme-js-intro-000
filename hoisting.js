eb2bee5d785c991c3ccda193111bf95c977befdf
diligent-utility-1811 <chrisogilvy@cox.net>
2019-02-10 02:51:48 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,5 +1,5 @@
-var lyric = "maybe";
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -13,17 +13,14 @@ function crazy() {
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  thisIsCrazy()();
-  }
+  };
+  thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
   sayMy();
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
 }
